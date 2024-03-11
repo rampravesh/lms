ec25c2d0dde72644fde818047c90c721c503d35e
Ram Pravesh Chauhan <rampraveshchauhan@L-C02FXP2MMD6M.local>
2024-03-11 20:39:20 +0530

    fixing unit testcases

--- src/test/java/com/loans/lms/service/impl/PlanServiceImplTest.java
+++ src/test/java/com/loans/lms/service/impl/PlanServiceImplTest.java
@@ -5,6 +5,7 @@ import com.loans.lms.dto.LoanPlansDTO;
 import com.loans.lms.entities.LoanPlans;
 import com.loans.lms.repository.BaseInterestRatesRepository;
 import com.loans.lms.repository.LoanPlansRepository;
+import com.loans.lms.service.RuleService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +20,7 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
 @ContextConfiguration(classes = {PlanServiceImpl.class})
@@ -31,10 +33,15 @@ class PlanServiceImplTest {
     private LoanPlansRepository loanPlansRepository;
 
     @MockBean
+    private RuleService ruleService;
+
+    @MockBean
     private ModelMapper modelMapper;
 
     @Autowired
     private PlanServiceImpl planServiceImpl;
+
+
 
     /**
      * Method under test: {@link PlanServiceImpl#getAllInterestRates()}
@@ -113,7 +120,12 @@ class PlanServiceImplTest {
         loanPlansDTO.setPrincipleAmount(1);
         loanPlansDTO.setTenure(1);
         loanPlansDTO.setTotalPayable(1);
+        LoanPlans loanPlans = Optional.ofNullable(DataUtils.getLoanPlans().get(0)).get();
+        Mockito.when(loanPlansRepository.save(loanPlans)).thenReturn(loanPlans);
+        Mockito.when(modelMapper.map(loanPlansDTO, LoanPlans.class)).thenReturn(loanPlans);
+        Mockito.when( modelMapper.map(loanPlans, LoanPlansDTO.class)).thenReturn(loanPlansDTO);
         Mockito.when(loanPlansRepository.findById(123)).thenReturn(java.util.Optional.ofNullable(DataUtils.getLoanPlans().get(0)));
+        Mockito.when(ruleService.getBaseRate(any())).thenReturn(5.5f);
         LoanPlansDTO update = this.planServiceImpl.update(123, loanPlansDTO);
         Assertions.assertEquals(loanPlansDTO.getPlanId(),update.getPlanId());
     }
